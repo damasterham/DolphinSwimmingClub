d9b85cd5459ce2828eb4359e8383874fad5f6f8f
damasterham <cmeisterham@gmail.com>
2016-05-20 11:23:25 +0200

    Added a main program class DolphinSwimmingClub.java, which is pretty much just console prompt that branches to our various functionality

--- DolphinSwimmingClub.java
+++ DolphinSwimmingClub.java
@@ -1,49 +1,128 @@
 import java.util.*;
 import java.io.*;
 
+//Chance
 public class DolphinSwimmingClub
-{
-
-   public static void main(String args)
-   {
-      Scanner console;
-      
-      console = new Scanner(System.in);
-      
-      
-   }
-   
-   public static int selectUser(Scanner console)
-   {
-      System.out.println("Hvem bruger systemet?");
-      System.out.println("1:Formand, 2:");
-      return 0;
-   }
-   
-   public static int inputInt(Scanner console)
+{            
+   private static int getInputInt(Scanner input, String mismatchError)
    {
       int value;
-   
-      if (console.hasNext())
+            
+      while (true)
       {
          try
          {
-            value = console.nextInt();
+            value = input.nextInt();
+            return value;          
          }
-         catch (Exception ex)
+         catch (InputMismatchException ex)
          {
+            System.out.println(mismatchError);
+            input.nextLine();
+         }
+      }      
+   }
+   
+   
+   private static int getInputIntWithinRange(Scanner input, int min, int max, String mismatchError, String rangeError)
+   {
+      int value;
+            
+      while (true)
+      {
+         try
+         {
+            value = input.nextInt();
+            
+            if (value >= min && value <= max)
+            {
+               return value;
+            }
+            else
+            {
+               System.out.println(rangeError);
+            }
             
          }
+         catch (InputMismatchException ex)
+         {
+            System.out.println(mismatchError);
+            input.nextLine();
+         }
+      }      
+   } 
+   
+   
+   private static void selectTrainerFunc(Scanner console)
+   {
+      int selection;
+      System.out.println("Hej Træner. Hvad vil du gerne gøre?");
+      System.out.println("1:Registrer nyt svømme resultat, 2:Se liste af top 5 svømmere");
+      
+      selection = getInputIntWithinRange(console, 1, 2, "Du skal indtaste et tal for at vælge", "Dine valgmuligheder er kun mellem 1 og 2");     
+      
+      switch (selection)    
+      {
+         //case 1: SwimResultHandler.createNewRestult; break;
+         case 2: SwimResultHandler.findBestSwimmers(); break;
       }
    }
    
-   private static int select(int value, int min, int max)
+   
+   private static void selectTreasurerFunc(Scanner console)
    {
+      int selection; 
+      System.out.println("Hej Kasserer. Hvad vil du gerne gøre?");
+      System.out.println("1:Se medlemmer i restance");
       
+      selection = getInputIntWithinRange(console,1 ,1,  "Du skal indtaste et tal for at vælge", "Der er kun en functionalitet, vælg nu bare 1");
       
-      if (value < min || value > max)
+      if (selection == 1)
       {
-         
+         //KassererFunc.bla();   Jeres handler her
+      }   
+   }
+   
+   
+   private static void selectChairmanFunc(Scanner console)
+   {
+      int selection; 
+      System.out.println("Hej formand. Hvad vil du gerne gøre?");
+      System.out.println("1:Registrer nyt medlem");
+      
+      selection = getInputIntWithinRange(console,1 ,1,  "Du skal indtaste et tal for at vælge", "Der er kun en functionalitet, vælg nu bare 1");
+      
+      if (selection == 1)
+      {
+         //FormandFunc.bla();       Jeres handler her
       }
    }
+   
+   
+   private static void selectUser(Scanner console)
+   {
+      int selection;
+      System.out.println("Hvem bruger systemet?");
+      System.out.println("1:Formand, 2:Kasserer, 3:Træner");      
+      selection = getInputIntWithinRange(console, 1, 3, "Du skal indtaste et tal for at vælge", "Dine valgmuligheder er kun mellem 1 og 3");     
+      
+      switch (selection)    
+      {
+         case 1: selectChairmanFunc(console); break;
+         case 2: selectTreasurerFunc(console); break;
+         case 3: selectTrainerFunc(console); break;
+      }   
+      
+   }
+   
+   
+   public static void main(String[] args)
+   {
+         Scanner console;
+         
+         console = new Scanner(System.in);
+         
+         selectUser(console);
+   }
+   
 }
